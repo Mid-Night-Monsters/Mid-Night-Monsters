faa69ad4bfa757c5cdd12aca693ade5240b62998
Akash Kandasamy <akashram953@gmail.com>
2021-10-19 11:56:32 +0530

    adding templates directory

--- smartRestaurant/settings.py
+++ smartRestaurant/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,7 +56,6 @@ ROOT_URLCONF = 'smartRestaurant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        'DIRS': [(os.path.join(BASE_DIR, 'templates')),],
     },
 ]
 
